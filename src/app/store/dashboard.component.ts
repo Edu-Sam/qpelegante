15877bf58ab3f6f59d8f94afceb80054ea3a5b09
Edu-Sam <edungugi20@gmail.com>
2021-07-21 17:02:53 +0300

    Product items

--- src/app/store/dashboard.component.ts
+++ src/app/store/dashboard.component.ts
@@ -1,4 +1,9 @@
 import {Component} from "@angular/core";
+import {Product} from "../model/product.model";
+import {ProductRepository} from "../model/product.repository";
+import {Cart } from "../model/cart.model";
+import {Router} from "@angular/router";
+
 
 
 @Component({
@@ -8,5 +13,53 @@ styleUrls:['dashboard.component.sass']
 })
 
 export class DashboardComponent{
+  public selectedCategory: string | undefined;
+  public productsPerPage=4;
+  public selectedPage=1;
 
+  constructor(private repository: ProductRepository,private cart: Cart,private router: Router){}
+
+  get products():Product[]{
+  /**  return this.repository.getProducts();
+      return this.repository.getProducts(this.selectedCategory);**/
+      let pageIndex=(this.selectedPage - 1) * this.productsPerPage
+      return this.repository.getProducts(this.selectedCategory).slice(pageIndex,pageIndex + this.productsPerPage);
+  }
+
+  get categories():string[]{
+    return this.repository.getCategories();
+  }
+
+  changeCategory(newCategory?:string){
+    if(newCategory!=undefined){
+      this.selectedCategory=newCategory;
+    }
+    else{
+      this.selectedCategory='';
+    }
+  }
+
+  changePage(newPage: number){
+    this.selectedPage=newPage;
+  }
+
+  changePageSize(newSize: number){
+    this.productsPerPage=Number(newSize);
+    this.changePage(1);
+
+  }
+
+  get pageCount():number{
+    return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage)
+  }
+
+  /**get pageNumbers():number[]{
+    return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage))
+           .fill(0).map((x,i)=>i + 1);
+  }**/
+
+  addProductToCart(product:Product){
+    this.cart.addLine(product);
+    this.router.navigateByUrl("/cart");
+  }
 }
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -3,9 +3,10 @@ import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {DashboardComponent} from "./dashboard.component";
 import {RouterModule} from "@angular/router";
+import {ModelModule} from "../model/model.module";
 
 @NgModule({
-  imports:[BrowserModule,FormsModule,RouterModule],
+  imports:[BrowserModule,FormsModule,RouterModule,ModelModule],
   declarations:[DashboardComponent],
   exports:[DashboardComponent],
 })
